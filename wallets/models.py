daf6c3fd8ac8a0256ccccb3131deac7ee3af6a1f
Milad Khoshdel <miladkhoshdel@gmail.com>
2024-05-23 10:50:55 +0330

    update wallet & Transaction model fields.

--- wallets/models.py
+++ wallets/models.py
@@ -2,17 +2,9 @@ import uuid
 
 from django.db import models
 from wallets.managers import WalletManager, TransactionManager
+from base.models import BaseModel
 
-class Transaction(models.Model):
-    amount = models.BigIntegerField()
-
-
-    objects = TransactionManager()
-    # todo: add fields if necessary
-    pass
-
-
-class Wallet(models.Model):
+class Wallet(BaseModel):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     balance = models.BigIntegerField(default=0)
 
@@ -24,3 +16,15 @@ class Wallet(models.Model):
 
     def __str__(self):
         return self.uuid
+
+class Transaction(BaseModel):
+    amount = models.BigIntegerField()
+    uuid = models.ForeignKey(Wallet,on_delete=models.CASCADE)
+    is_withdrawal = models.BooleanField(default=False)
+
+    objects = TransactionManager()
+
+    def __str__(self):
+        return f"{'Withdrawal' if self.is_withdrawal else 'Deposit'} of {self.amount} for {self.wallet.uuid}"
+
+
--- wallets/serializers.py
+++ wallets/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
-
-from wallets.models import Wallet
+from wallets.models import Wallet, Transaction
 
 
 class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wallet
-        fields = ("uuid", "balance")
-        read_only_fields = ("uuid", "balance")
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
