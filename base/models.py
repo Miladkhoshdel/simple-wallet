2d5f336a81270b5df2fa37e14fb06b992c55be8b
Milad Khoshdel <miladkhoshdel@gmail.com>
2024-05-23 10:44:14 +0330

    base app with base model and model managers created

---
+++ base/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Base Model
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        abstract = True
--- wallets/models.py
+++ wallets/models.py
@@ -1,10 +1,13 @@
 import uuid
 
 from django.db import models
-
+from wallets.managers import WalletManager, TransactionManager
 
 class Transaction(models.Model):
     amount = models.BigIntegerField()
+
+
+    objects = TransactionManager()
     # todo: add fields if necessary
     pass
 
@@ -13,7 +16,11 @@ class Wallet(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     balance = models.BigIntegerField(default=0)
 
+    objects = WalletManager()
+
     def deposit(self, amount: int):
         # todo: deposit the amount into this wallet
         pass
 
+    def __str__(self):
+        return self.uuid
